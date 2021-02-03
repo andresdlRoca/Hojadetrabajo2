b2ca72958217f243e7e21aaa347b4d6d1c70ac82
Andres de la Roca <andresdelaroca11@gmail.com>
2021-02-02 19:48:49 -0600

    Se agregaron comentarios y metodos

--- CalculadoraGeneral.java
+++ CalculadoraGeneral.java
@@ -1,3 +1,11 @@
-public interface CalculadoraGeneral {
+/**
+ * Interfaz que contiene los metodos para el funcionamiento de la calculadora
+ * de notacion postfix
+ * @author Andres de la Roca
+ */
+
+public interface CalculadoraGeneral<T> {
     
+    public String Calculo(String texto);
+
 }
--- Stack.java
+++ Stack.java
@@ -1,3 +1,40 @@
-public interface Stack {
+/**
+ * Clase de interfaz que tendra los metodos que usara la clase de stack para el almacenamiento
+ * de la calculadora. 
+ * @author Andres de la Roca
+ */
+
+public interface Stack<T> {
     
+    /**
+     * Permite almacenar dentro del stack un objeto
+     * @param num
+     * @return T
+     */
+    public T Push(int num);
+
+    /**
+     * Permite sacar del stack el ultimo objeto que se ingreso
+     * @return
+     */
+    public T Pop();
+
+    /**
+     * Devuelve un valor true si el almacenamiento stack esta vacio y false si esta lleno
+     * @return
+     */
+    public boolean Empty();
+
+    /**
+     * Devuelve el ultimo objeto que se ingreso al stack
+     * @return
+     */
+    public T Peek();
+
+    /**
+     * Devuelve el tamaño del almacenamiento stack
+     * @return
+     */
+    public int Size();
+
 }
